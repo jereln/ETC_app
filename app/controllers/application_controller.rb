9e7ba01e6dd85763d7f89e89afea5af995c92150
Jerel Navarrete <navarrete.jerel@gmail.com>
2014-12-11 14:53:29 -0800

    removed bourbon

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,47 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  before_action :configure_permitted_parameters, if: :devise_controller?
   protect_from_forgery with: :exception
+
+  # protected
+  # def not_logged_in!
+  #   if person_signed_in?
+  #     super
+  #   else
+  #     redirect_to '/sign_in', :notice => 'if you want to add a notice'
+  #   end
+  # end
 
   private
   def after_sign_out_path_for(person)
   	new_person_session_path
+  end
+
+  protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) do
+      |u| u.permit(:email,
+                   :first_name,
+                   :last_name,
+                   :gender,
+                   :password,
+                   :password_confirmation,
+                   :remember_me)
+    end
+    devise_parameter_sanitizer.for(:account_update) do
+      |u| u.permit(:email,
+                   :first_name,
+                   :last_name,
+                   :gender,
+                   :birthdate,
+                   :address,
+                   :city,
+                   :state,
+                   :zip_code,
+                   :password,
+                   :password_confirmation,
+                   :current_password)
+    end
   end
 end
--- app/models/person.rb
+++ app/models/person.rb
@@ -4,5 +4,16 @@ class Person < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 	self.table_name = 'tblPerson'
+
+	belongs_to :abilities
+
 	alias_attribute :email, :Email
+	alias_attribute :first_name, :FirstName
+	alias_attribute :last_name, :LastName
+	alias_attribute :gender, :Gender
+	alias_attribute :birthdate, :BirthDate
+	alias_attribute :address, :Address
+	alias_attribute :city, :City
+	alias_attribute :state, :State
+	alias_attribute :zip_code, :ZipCode
 end
