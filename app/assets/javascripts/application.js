35b80c8bdce5bd08173f279896cccfb5c24d0a5e
Bess Navarrete <bess.navarrete@gmail.com>
2014-12-19 15:12:10 -0800

    set up javascript for checking contact info

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,10 +15,10 @@
 //= require_tree .
 
 function fSetBestContact() {
-    var HomePhone = document.getElementById('<%= HomePhoneTextBox.ClientID %>').value;
-    var WorkPhone = document.getElementById('<%= WorkPhoneTextBox.ClientID %>').value;
-    var CellPhone = document.getElementById('<%= CellPhoneTextBox.ClientID %>').value;
-    var dropDown = document.getElementById('<%= BestContactDropDownList.ClientID %>');
+    var HomePhone = document.getElementById('HomePhoneTextBox').value;
+    var WorkPhone = document.getElementById('WorkPhoneTextBox').value;
+    var CellPhone = document.getElementById('CellPhoneTextBox').value;
+    var dropDown = document.getElementById('person_cell_phone');
     var length = dropDown.options.length;
     if (HomePhone != '' && WorkPhone == '' && CellPhone == '') {
         for (var i = 0; i < length; i++) {
@@ -47,10 +47,10 @@ function fSetBestContact() {
 }
 function fCheckBestContact() {
     // Get values from the three text boxes and assign to variables
-    var HomePhone = document.getElementById('<%= HomePhoneTextBox.ClientID %>').value;
-    var WorkPhone = document.getElementById('<%= WorkPhoneTextBox.ClientID %>').value;
-    var CellPhone = document.getElementById('<%= CellPhoneTextBox.ClientID %>').value;
-    var dropDown = document.getElementById('<%= BestContactDropDownList.ClientID %>');
+    var HomePhone = document.getElementById('HomePhoneTextBox').value;
+    var WorkPhone = document.getElementById('WorkPhoneTextBox').value;
+    var CellPhone = document.getElementById('CellPhoneTextBox').value;
+    var dropDown = document.getElementById('person_cell_phone');
     var length = dropDown.options.length;
     // Get chosen value in combo box and assign to variable
     for (var i = 0; i < length; i++) {
@@ -61,14 +61,14 @@ function fCheckBestContact() {
     }
     if (dropDownValue == 'Home' && HomePhone == '') {
         alert('No home phone number has been entered.');
-        document.getElementById('<%= HomePhoneTextBox.ClientID %>').focus();
+        document.getElementById('HomePhoneTextBox').focus();
     }
     else if (dropDownValue == 'Work' && WorkPhone == '') {
         alert('No work phone number has been entered.');
-        document.getElementById('<%= WorkPhoneTextBox.ClientID %>').focus();
+        document.getElementById('WorkPhoneTextBox').focus();
     }
     else if (dropDownValue == 'Cell' && CellPhone == '') {
         alert('No cell phone number has been entered.');
-        document.getElementById('<%= CellPhoneTextBox.ClientID %>').focus();
+        document.getElementById('CellPhoneTextBox').focus();
     }
 }
